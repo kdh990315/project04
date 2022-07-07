48e89ed60a26a129582e621797aea99677e57913
kdh990315 <kdh990315@naver.com>
2022-07-07 16:54:07 +0900

    ddww

--- js/sub_scroll.js
+++ js/sub_scroll.js
@@ -1,56 +1,104 @@
 $(function () {
     $(window).scroll(function () {
+        if (window.matchMedia("(min-width: 1200px)").matches) {
+            // 오른쪽 info 이미지
+            for(var i = 0; i < 6; i++) {
+                var $img_box = $(".info_box_img" + i);
+                var $info_menu_box = $(".info_nav_box");
+                var $info_menu_on = $(".info_ul > li").eq(i);
 
-        // 오른쪽 info 이미지
-        for(var i = 0; i < 6; i++) {
-            var $img_box = $(".info_box_img" + i);
-            var $info_menu_box = $(".info_nav_box");
-            var $info_menu_on = $(".info_ul > li").eq(i);
-
-            
-
-            if($(this).scrollTop() <= $img_box.offset().top - 699) {
-                $img_box.css({
-                    "width" : "800px"
-                });
-
-                $info_menu_on.removeClass("on");
-            }
-
-            if($(this).scrollTop() > $img_box.offset().top -700) {
-                $img_box.css({
-                    "width" : "58vw"
-                });
                 
-                $info_menu_on.addClass("on");
 
-            }   
-            if(($(this).scrollTop() > $img_box.offset().top + 300)) {
-                $img_box.css({
-                    "width" : "800px"
-                });
+                if($(this).scrollTop() <= $img_box.offset().top - 699) {
+                    $img_box.css({
+                        "width" : "800px"
+                    });
 
-                $info_menu_on.removeClass("on");
+                    $info_menu_on.removeClass("on");
+                }
+
+                if($(this).scrollTop() > $img_box.offset().top -700) {
+                    $img_box.css({
+                        "width" : "58vw"
+                    });
+                    
+                    $info_menu_on.addClass("on");
+
+                }   
+                if(($(this).scrollTop() > $img_box.offset().top + 300)) {
+                    $img_box.css({
+                        "width" : "800px"
+                    });
+
+                    $info_menu_on.removeClass("on");
+                }
+
+
+
+
+                // 왼쪽 menu
+                if($(this).scrollTop() > $info_menu_box.offset().top -478) {
+                    $info_menu_box.css({
+                        "position" : "sticky",
+                        "top": "150px"
+                    });
+                }
+                if($(this).scrollTop() <= $info_menu_box.offset().top -577) {
+                    $info_menu_box.css({
+                        "position" : "absolute"
+                    });
+                }
             }
+        } else {
+            for(var i = 0; i < 6; i++) {
+                var $img_box = $(".info_box_img" + i);
+                var $info_menu_box = $(".info_nav_box");
+                var $info_menu_on = $(".info_ul > li").eq(i);
+
+                
+
+                if($(this).scrollTop() <= $img_box.offset().top - 699) {
+                    // $img_box.css({
+                    //     "width" : "800px"
+                    // });
+
+                    $info_menu_on.removeClass("on");
+                }
+
+                if($(this).scrollTop() > $img_box.offset().top -700) {
+                    // $img_box.css({
+                    //     "width" : "58vw"
+                    // });
+                    
+                    $info_menu_on.addClass("on");
+
+                }   
+                if(($(this).scrollTop() > $img_box.offset().top + 300)) {
+                    // $img_box.css({
+                    //     "width" : "800px"
+                    // });
+
+                    $info_menu_on.removeClass("on");
+                }
 
 
 
 
-            // 왼쪽 menu
-            if($(this).scrollTop() > $info_menu_box.offset().top -478) {
-                $info_menu_box.css({
-                    "position" : "sticky",
-                    "top": "150px"
-                });
+                // 왼쪽 menu
+                if($(this).scrollTop() > $info_menu_box.offset().top -478) {
+                    $info_menu_box.css({
+                        "position" : "sticky",
+                        "top": "150px"
+                    });
+                }
+                if($(this).scrollTop() <= $info_menu_box.offset().top -577) {
+                    $info_menu_box.css({
+                        "position" : "absolute"
+                    });
+                }
             }
-            if($(this).scrollTop() <= $info_menu_box.offset().top -577) {
-                $info_menu_box.css({
-                    "position" : "absolute"
-                });
-            }
-
-
         }
+
         
     });
 });
